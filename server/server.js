a8271b4a6ea60a98c7eb99bc8b08d0c5be1b760a
Craig Clayton <craig__clayton@outlook.com>
2025-01-08 18:34:13 +0000

    add a get query to see if something works

--- server/server.js
+++ server/server.js
@@ -1,3 +1,27 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+app.get('/', (req, res) => {
+  res.send(
+    '<h1 style="color: red; text-align: center;">This is the root route</h1>'
+  );
+});
+
+app.get('/flamingo-images', async (req, res) => {
+  const API = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&query=flamingo`;
+  const response = await fetch(API);
+  const data = await response.json();
+
+  res.json(data.results);
+});
+
+const PORT = 8080;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
