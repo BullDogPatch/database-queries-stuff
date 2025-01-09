4803656f64f4ea9d42d64cf0720855cbf4ce691b
Craig Clayton <craig__clayton@outlook.com>
2025-01-09 10:30:55 +0000

    rendering players on to client

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,14 @@
+const playersContainer = document.querySelector('.players-container');
+
 const getSomePlayersFromDatabase = async () => {
   const response = await fetch('http://localhost:8080/players');
   const data = await response.json();
   console.log(data);
+  data.forEach((player) => {
+    const p = document.createElement('p');
+    p.textContent = player.name;
+    playersContainer.appendChild(p);
+  });
 };
 
 getSomePlayersFromDatabase();
