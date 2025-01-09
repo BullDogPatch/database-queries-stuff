28026b40079005627854e238bcfc55bc841bcd9a
Craig Clayton <craig__clayton@outlook.com>
2025-01-09 10:56:40 +0000

    done what I wanted to do with client for now

--- client/src/main.js
+++ client/src/main.js
@@ -5,9 +5,22 @@ const getSomePlayersFromDatabase = async () => {
   const data = await response.json();
   console.log(data);
   data.forEach((player) => {
-    const p = document.createElement('p');
-    p.textContent = player.name;
-    playersContainer.appendChild(p);
+    const div = document.createElement('div');
+    div.className = 'player';
+    div.style.border = '1px solid darkgrey';
+    const h3 = document.createElement('h3');
+    const shirtNum = document.createElement('p');
+    const playerPos = document.createElement('p');
+    const nationality = document.createElement('p');
+    h3.textContent = `${player.name} ${player.is_captain ? '(C)' : ''}`;
+    shirtNum.textContent = `# ${player.shirt_number}`;
+    playerPos.textContent = `Position: ${player.position}`;
+    nationality.textContent = `Nationality: ${player.nationality}`;
+    div.appendChild(h3);
+    div.appendChild(shirtNum);
+    div.appendChild(playerPos);
+    div.appendChild(nationality);
+    playersContainer.appendChild(div);
   });
 };
 
